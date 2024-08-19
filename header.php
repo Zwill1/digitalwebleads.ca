e54a0e3defd42b1e4eec04237f5d49c5faa4919b
zwill <zwill_1@hotmail.com>
2024-08-19 00:12:31 -0600

    minor edits to header template

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <!-- <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title> -->
-    <?php wp_head() ?>
+    <?php wp_head(); ?>
 </head>
 <body>
 <p class="bg-red-100">header</p>
