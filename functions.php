d231ecd9f220b0f2f509a31e5c866064231a3f47
zwill <zwill_1@hotmail.com>
2024-08-26 01:29:01 -0600

    adding featured image support to WP

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ add_action('wp_enqueue_scripts', 'digitalweb_styles');
 
 function digitalweb_features(){
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'digitalweb_features');
