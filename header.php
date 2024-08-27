aab89d29d96ff41cd218ded5a826d895e51833eb
zwill <zwill_1@hotmail.com>
2024-08-27 01:06:52 -0600

    adding stylesheet with header content layout

--- header.php
+++ header.php
@@ -2,9 +2,22 @@
 <html lang="en">
 <head>
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+    <!-- <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" /> -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
 </head>
 <body>
-<section class="w-100 bg-black">
-    <p class="text-center text-white">header</p>
+<section class="w-100 bg-black flex justify-start">
+    <div class="text-left text-white w-1/2 flex flex-row ml-1">
+        <p class="ml-1 mr-1">header</p>
+        <p class="ml-1 mr-1">header</p>
+    </div>
+    <div class="text-white w-1/2 flex mr-1 justify-end">
+        <p class="ml-1 mr-1">
+            <i class="ri-facebook-fill"></i>
+        </p>
+        <p class="ml-1 mr-1">
+            <i class="ri-instagram-line"></i>
+        </p>
+        <p class="ml-1 mr-1">Contact Us</p>
+    </div>
 </section>
