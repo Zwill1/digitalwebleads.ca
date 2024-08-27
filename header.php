77656f27f2335801583b332aa575c2929864191f
zwill <zwill_1@hotmail.com>
2024-08-27 01:19:14 -0600

    updating header with target and hrefs

--- header.php
+++ header.php
@@ -8,16 +8,26 @@
 <body>
 <section class="w-100 bg-black flex justify-start">
     <div class="text-left text-white w-1/2 flex flex-row ml-1">
-        <p class="ml-1 mr-1">header</p>
-        <p class="ml-1 mr-1">header</p>
+        <p class="ml-1 mr-1">
+            <a href="#">123-456-7890</a>
+        </p>
+        <p class="ml-1 mr-1">
+            <a href="#">Email@email.com</a>
+        </p>
     </div>
     <div class="text-white w-1/2 flex mr-1 justify-end">
         <p class="ml-1 mr-1">
-            <i class="ri-facebook-fill"></i>
+            <a href="#" target="_blank">
+                <i class="ri-facebook-fill"></i>
+        </a>
         </p>
         <p class="ml-1 mr-1">
-            <i class="ri-instagram-line"></i>
+            <a href="#" target="_blank">
+                <i class="ri-instagram-line"></i>
+            </a>
         </p>
-        <p class="ml-1 mr-1">Contact Us</p>
+        <p class="ml-1 mr-1">
+            <a href="#contact">Contact Us</a>
+        </p>
     </div>
 </section>
