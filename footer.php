c77a4cab45ec1b96e47c2d83d816291535426eb8
zwill <zwill_1@hotmail.com>
2024-08-27 23:11:41 -0600

    set content layout width and placements

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
-<p>footer</p>
+<section class="w-100 bg-black">
+    <div class="w-10/12 text-white flex mx-auto">
+        <div class="w-1/3">1</div>
+        <div class="w-1/3">1</div>
+        <div class="w-1/3">1</div>
+    </div>
+</section>
 <?php wp_footer(); ?>
 </body>
 </html>
