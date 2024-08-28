b7f3537a78a543c68663ff3c35e8b0bf2a91b832
zwill <zwill_1@hotmail.com>
2024-08-27 23:11:18 -0600

    updating header with better content layout css

--- header.php
+++ header.php
@@ -6,28 +6,30 @@
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
 </head>
 <body>
-<section class="w-100 bg-black flex justify-start">
-    <div class="text-left text-white w-1/2 flex flex-row ml-1">
-        <p class="ml-1 mr-1">
-            <a href="#">123-456-7890</a>
-        </p>
-        <p class="ml-1 mr-1">
-            <a href="#">Email@email.com</a>
-        </p>
-    </div>
-    <div class="text-white w-1/2 flex mr-1 justify-end">
-        <p class="ml-1 mr-1">
-            <a href="#" target="_blank">
-                <i class="ri-facebook-fill"></i>
-        </a>
-        </p>
-        <p class="ml-1 mr-1">
-            <a href="#" target="_blank">
-                <i class="ri-instagram-line"></i>
+<section id="top-header-bar" class="w-100 bg-black flex justify-start">
+    <div class="w-10/12 text-white flex mx-auto">
+        <div class="w-1/2 flex">
+            <p class="ml-1 mr-1">
+                <a href="#">123-456-7890</a>
+            </p>
+            <p class="ml-1 mr-1">
+                <a href="#">Email@email.com</a>
+            </p>
+        </div>
+        <div class="w-1/2 flex justify-end">
+            <p class="ml-1 mr-1">
+                <a href="#" target="_blank">
+                    <i class="ri-facebook-fill"></i>
             </a>
-        </p>
-        <p class="ml-1 mr-1">
-            <a href="#contact">Contact Us</a>
-        </p>
+            </p>
+            <p class="ml-1 mr-1">
+                <a href="#" target="_blank">
+                    <i class="ri-instagram-line"></i>
+                </a>
+            </p>
+            <p class="ml-1 mr-1">
+                <a href="#contact">Contact Us</a>
+            </p>
+        </div>
     </div>
 </section>
